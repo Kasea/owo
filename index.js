2d9af724ba254c54efd6ac2bb5770295f8ad87b1
Kasea <>
2018-12-13 19:31:00 +0100

    advancing humanity one step at a time

--- index.js
+++ index.js
@@ -8,19 +8,36 @@ const phrases = {
     "(?:r|l)": "w",
     "(?:R|L)": "W",
 
-    // replace [a,e,i,o,u]y%
+    // replace n[a,e,i,o,u]y%
     "n([aeiou])": "ny$1",
     "N([aeiou])": "Ny$1",
     "N([AEIOU])": "Ny$1",
+
+    // replace k[a,e,i,o,u]y%
+    "k([aeiou])": "ky$1",
+    "K([aeiou])": "Ky$1",
+    "K([AEIOU])": "Ky$1",
 
     // replace "ove" with "uv"
     "ove": "uv",
 
     // replace "boy" with "boi"
     "boy": "boi",
+
+    // replace "nya" with "nya~"
+    "nya([< ])": "nya~$1",
+    "Nya([< ])": "Nya~$1",
+    "NYA([< ])": "NYA~$1",
+
+    // replace "kya" with "kya~"
+    "kya([< ])": "kya~$1",
+    "Kya([< ])": "Kya~$1",
+    "KYA([< ])": "KYA~$1",
 };
 
-module.exports = function OwO(mod){
+module.exports = function OwO(mod) {
+    const DEBUG = false;
+
     function OwOify(msg) {
         for(let rgx in phrases) msg = msg.replace(new RegExp(rgx, 'g'), phrases[rgx]);
 
@@ -30,7 +47,29 @@ module.exports = function OwO(mod){
         return msg;
     }
 
-    mod.hook('S_CHAT', 2, e=> ((e.message = OwOify(e.message)) && (e.authorName = OwOify(e.authorName)) && true));
-    mod.hook('S_WHISPER', 2, e=> ((e.message = OwOify(e.message)) && ((mod.game.me.name == e.authorName) ? (e.recipient = OwOify(e.recipient)) : (e.authorName = OwOify(e.authorName))) && true));
-    mod.hook('C_CHAT', 1, e=> ((e.message = OwOify(e.message)) && true));
+    function foo(type, e) {
+        if(e.message.includes("ChatLinkAction")) return;
+        if(DEBUG) console.log(e);
+
+        e.message = OwOify(e.message);
+        switch(type) {
+            case "S_CHAT": {
+                e.authorName = OwOify(e.authorName)
+                break;
+            }
+
+            case "S_WHISPER": {
+                if(mod.game.me.name == e.authorName) e.recipient = OwOify(e.recipient)
+                else e.authorName = OwOify(e.authorName)
+                break;
+            }
+        }
+        
+        if(DEBUG) console.log(e);
+        return true;
+    }
+
+    mod.hook('S_CHAT', 2, foo.bind(null, "S_CHAT"));
+    mod.hook('S_WHISPER', 2, foo.bind(null, "S_WHISPER"));
+    mod.hook('C_CHAT', 1, foo.bind(null, "C_CHAT"));
 }
